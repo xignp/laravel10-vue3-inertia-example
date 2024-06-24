a9ba90405fcb4123fa8b58cbc27088635915926c
XIGNp <xign4432@gmail.com>
2024-06-25 00:45:07 +0900

    add components

---
+++ resources/js/Components/Alert.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { computed } from 'vue'
+const props = defineProps({
+  color: String,
+})
+
+const commonClass = 'p-4 mb-4 rounded-lg'
+
+const colorClass = computed(() => {
+  switch (props.color) {
+    case 'success':
+      return 'bg-green-100 text-green-800'
+    case 'error':
+      return 'bg-red-100 text-red-800'
+    default:
+      return 'bg-green-100 text-green-800'
+  }
+})
+</script>
+
+<template>
+  <div :class="`${commonClass} ${colorClass}`">
+    <slot />
+  </div>
+</template>
---
+++ resources/js/Components/Button.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { computed } from 'vue'
+const props = defineProps({
+  color: String,
+})
+
+const commonClass = 'rounded-lg px-5 py-2.5'
+
+const colorClass = computed(() => {
+  switch (props.color) {
+    case 'primary':
+      return 'bg-blue-700 hover:bg-blue-800 text-white'
+    case 'secondary':
+      return 'bg-white hover:bg-gray-100 text-black hover:text-blue-700'
+    case 'success':
+      return 'bg-green-600 hover:bg-green-700 text-white'
+    case 'error':
+      return 'bg-red-600 hover:bg-red-700 text-white'
+    default:
+      return 'bg-blue-700 hover:bg-blue-800 text-white'
+  }
+})
+</script>
+
+<template>
+  <button :class="`${commonClass} ${colorClass}`"><slot /></button>
+</template>
